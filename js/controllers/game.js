09f6f8bff7ad1d6445ccd951b82de2643490b007
Alonso Vidales <alonso.vidales@tras2.es>
2012-04-05 09:39:10 +0200

    Commenting code

--- js/controllers/game.js
+++ js/controllers/game.js
@@ -1,11 +1,30 @@
+/**
+  * Author: Alonso Vidales <alonso.vidales@tras2.es>
+  * Date: 2012-03-26
+  *
+  * This class is used to control all the game features level by level
+  *
+  * @see config.game
+  *
+  */
 var Game_Controller = (function() {
+	// Will contain the main canvas div element
 	var mainCanvas = null;
+	// Will contain the Compressor_Controller object
 	var compressor = null;
+	// Will contain the Player_Controller object
 	var player = null;
+	// Will contain the Text_Tool object for the score
 	var scoreText = null;
+	// The number of the current level will be stored and initialized into localStorage
 	var currentLevel = 1;
+	// The the current score will be stored and initialized into localStorage
 	var currentScore = 0;
 
+	/**
+	  * This method will return the last played level from localStorage, or 1 if is not defined
+	  *
+	  */
 	var getLevel = function() {
 		var level = localStorage.getItem('lastLevel');
 		if (level === null) {
@@ -15,19 +34,38 @@ var Game_Controller = (function() {
 		return parseInt(level, 10);
 	};
 
+	/**
+	  * This method store the current level into localStorage
+	  *
+	  */
 	var saveLevel = function() {
 		localStorage.setItem('lastLevel', currentLevel);
 	};
 
+	/**
+	  * Adds the inScore points to the current score and updates the score text
+	  *
+	  * @param inScore <int>: The points to add to the current score
+	  *
+	  */
 	var setScore = function(inScore) {
 		currentScore = inScore;
 		scoreText.setText('score: ' + inScore);
 	};
 
+	/**
+	  * This method store the current score into localStorage
+	  *
+	  */
 	var saveScore = function() {
 		localStorage.setItem('totalScore', currentScore);
 	};
 
+	/**
+	  * This method return from localStorage the last score, or zero if
+	  * it is not defined
+	  *
+	  */
 	var getScore = function() {
 		var score = localStorage.getItem('totalScore');
 		if (score === null) {
@@ -37,11 +75,17 @@ var Game_Controller = (function() {
 		return parseInt(score, 10);
 	};
 
+	/**
+	  * This method shows the game credits into a wood box
+	  *
+	  */
 	var showCredits = function() {
+		// Creates the wood background
 		var backgroundDiv = document.createElement("div");
 		backgroundDiv.classList.add('credits_background');
 		mainCanvas.appendChild(backgroundDiv);
 
+		// Creates the text and adds the link
 		var credits = new Text_Tool('created by:');
 		credits.init(18, 437);
 		credits = new Text_Tool('alonso vidales');
@@ -52,6 +96,7 @@ var Game_Controller = (function() {
 		});
 	};
 
+	// Public scope
 	var my = {
 		addToScore: function(inPoints) {
 			setScore(currentScore + inPoints);
