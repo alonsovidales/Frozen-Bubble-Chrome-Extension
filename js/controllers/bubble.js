8c08202d5c21a1d38ae30a0ffa2e43e60b485047
Alonso Vidales <alonso.vidales@tras2.es>
2012-04-07 20:45:35 +0200

    Commenting the code

--- js/controllers/bubble.js
+++ js/controllers/bubble.js
@@ -2,19 +2,40 @@
   * Author: Alonso Vidales <alonso.vidales@tras2.es>
   * Date: 2012-03-26
   *
-  * This class is used to control all the bubble features, the image, move it, check collisions, etc
+  * This class is used to control all the bubble features,
+  * the image, move it, check collisions, etc
   *
   * @see config.game
   *
   */
 var Bullble_Controller = (function(inX, inY, inType) {
+	// Will contain the image, @see AnimatedImage_Tool
 	var image = null;
+	// The type as a integer of the bubble (the color)
 	var type = inType;
 
+	/**
+	  * The next method returns the point where the bubble will bounce
+	  * according to the given angle in degrees  and the laterals of
+	  * the pitch
+	  *
+	  * @param inAngle <int>: The degrees that the bubble have taking the
+	  *	Y axe as reference
+	  *
+	  * @return <object>: Return the cordinates of the point where the bounce
+	  * 	will be produced in pixel in an object with the next strucutre:
+	  *		{
+	  *			x: <int>,
+	  *			y: <int>}
+	  *
+	  */
 	var calcBounce = function(inAngle) {
 		var point = null;
+		// Convert the angle to radians
 		var angle = (90 - Math.abs(inAngle)) * Math.PI / 180;
 
+		// Check if the bubble goes to the right in order to calculate the distance
+		// of the X axis
 		if (inAngle > 0) {
 			var b = config.ballsField.topRight.x - image.getX() - config.bubbles.width;
 			var a = b * Math.tan(angle);
@@ -36,16 +57,35 @@ var Bullble_Controller = (function(inX, inY, inType) {
 		return point;
 	};
 
+	// Public scope
 	var my = {
+		/**
+		  * Destroy the bubble moving it first to a given coordinates, and later to
+		  * the bottom of the window, after all the movements destroy the element
+		  *
+		  * @param inJumpX <int>: The number in pixels of the X axe to move the ball
+		  * @param inJumpY <int>: The number in pixels of the Y axe to move the ball
+		  *
+		  */
 		destroy: function(inJumpX, inJumpY) {
+			// First jump
 			my.moveTo(inJumpX, inJumpY, function() {
+				// Move to bottom
 				my.moveTo(inJumpX, config.windowConf.height, function() {
+					// Destroy the bubble
 					image.hide();
 					image = null;
 				});
 			});
 		},
 
+		/**
+		  * Launch a bubble from the shooter using a determinate angle
+		  *
+		  * @param inAngle <int>: The initial angle that the shooter have at the moment of the launch
+		  * @param inAngle <object Compressor_Controller>: The object of the main compressor
+		  *
+		  */
 		shoot: function(inAngle, inCompressor) {
 			var targetPoint = calcBounce(inAngle);
 
@@ -54,18 +94,46 @@ var Bullble_Controller = (function(inX, inY, inType) {
 			}, inCompressor.checkCollision);
 		},
 
+		/**
+		  * Move the bubble to a determinate position, sliding the bubble till the objective
+		  *
+		  * @param inX <int>: The pixels of the X axe to move the bubble where
+		  * @param inY <int>: The pixels of the Y axe to move the bubble where
+		  * @param inCallBack <function>: The function to be called after the movement ends
+		  * @param inCheckCollFunc <function>: The function to call each step the bubble
+		  *	gives till the objective is reached
+		  *
+		  */
 		moveTo: function(inX, inY, inCallBack, inCheckCollFunc) {
 			image.moveTo(inX, inY, config.bubbles.loopTime, inCallBack, config.bubbles.steepPx, inCheckCollFunc, this);
 		},
 
+		/**
+		  * Returns an integer as the type of the bubble
+		  *
+		  * @return <int>: The number assigned to this bubble
+		  *
+		  */
 		getType: function() {
 			return type;
 		},
 
+		/**
+		  * Returns the object of the animated image the represents the bubble
+		  *
+		  * @see AnimatedImage_Tool
+		  * @return <AnimatedImage_Tool>: The object to the bubble element
+		  *
+		  */
 		getImage: function() {
 			return image;
 		},
 
+		/**
+		  * This method creates an image to add it as mask over the bubble image
+		  * that creates the frozen ilusion
+		  *
+		  */
 		froze: function() {
 			var frozeImage = new AnimatedImage_Tool('bubble_frozzen');
 			frozeImage.init();
@@ -73,10 +141,19 @@ var Bullble_Controller = (function(inX, inY, inType) {
 			frozeImage.show();
 		},
 
+		/**
+		  * Stops all the animations of the bubble
+		  *
+		  */
 		stop: function() {
 			image.stop();
 		},
 
+		/**
+		  * Init the bubble, create the object of the bubble, and shows it into the
+		  * position specified ad the constructor
+		  *
+		  */
 		init: function() {
 			if (type === undefined) {
 				type = Math.floor(Math.random() * config.bubbles.totalTypes);
